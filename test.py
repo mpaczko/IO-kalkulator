211a7592189e40a2fc044d6da961171e5f2e442e
Michal <michalpaczko@gmail.com>
2022-04-02 13:45:24 +0200

    control commit2

--- test.py
+++ test.py
@@ -3,14 +3,7 @@
 def hello(name):
 	return "Hello" + str(name)
 
-def dodaj(a,b):
-	wynik = float(a) + float(b)
-	return wynik
-
-pierwsza = input()
-druga = input()
-
-print (dodaj(pierwsza, druga))
+print("Hello world")
 
 
  

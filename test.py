1272438002b341e127f56f2649f27f6f144433a5
Michal <michalpaczko@gmail.com>
2022-04-02 13:35:17 +0200

    control commit

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
+# To bedzie plik na ktorym bedziemy pracowali z repo
+
 def hello(name):
 	return "Hello" + str(name)
 
-print("Hello world")
+def dodaj(a,b):
+	wynik = float(a) + float(b)
+	return wynik
+
+pierwsza = input()
+druga = input()
+
+print (dodaj(pierwsza, druga))
+
+
+ 
